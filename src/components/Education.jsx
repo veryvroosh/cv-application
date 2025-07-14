aa1000b332914b89df5dfd3e76eb807d59ad88f9
vroosh <zaidsafadi.2005@gmail.com>
2025-07-14 06:12:57 +0300

    form completed for edit general

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,8 +1,28 @@
+import {useState} from "react";
+
+function EduSection({ form, setForm }) {
+
+}
+
 function Education() {
+    const [showMore, setShowMore] = useState(false);
+    const [form, setForm] = useState({
+
+    })
+
+    function handleShowMore() {
+        setShowMore(!showMore)
+    }
+
     return (
         <div className="edit-card">
-            <h2>Education</h2>
-            <button>V</button>
+            <div className="main-card">
+                <h2>Education</h2>
+                <button onClick={handleShowMore}>
+                    {showMore ? 'Hide' : 'Show'}
+                </button>
+            </div>
+            {showMore && <EduSection form={form} setForm={setForm}/>}
         </div>
     )
 }
--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,10 +1,88 @@
+import { useState } from 'react';
+import '../styles/general.css'
+
+function ToggledGeneral({ form, setForm }) {
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <div className="general-toggled">
+            <form className="general-form" onSubmit={handleSubmit}>
+                <label>
+                    First Name:{' '}
+                    <input
+                        value={form.firstName}
+                        onChange={e => setForm({...form, firstName: e.target.value})}
+                    />
+                </label>
+                <label>
+                    Last Name:{' '}
+                    <input
+                        value={form.lastName}
+                        onChange={e => setForm({...form, lastName: e.target.value})}
+                    />
+                </label>
+                <label>
+                    City & Country:{' '}
+                    <input
+                        value={form.cityCountry}
+                        onChange={e => setForm({...form, cityCountry: e.target.value})}
+                    />
+                </label>
+                <label>
+                    E-Mail:{' '}
+                    <input
+                        value={form.email}
+                        onChange={e => setForm({...form, email: e.target.value})}
+                    />
+                </label>
+                <label>
+                    Github:{' '}
+                    <input
+                        value={form.github}
+                        onChange={e => setForm({...form, github: e.target.value})}
+                    />
+                </label>
+                <label>
+                    LinkedIn:{' '}
+                    <input
+                        value={form.linkedin}
+                        onChange={e => setForm({...form, linkedin: e.target.value})}
+                    />
+                </label>
+                <button className='apply-button' type='submit'>Apply</button>
+            </form>
+        </div>
+    )
+}
+
 function General() {
+
+    const [showMore, setShowMore] = useState(false);
+    const [form, setForm] = useState({
+        firstName: 'Zaid',
+        lastName: 'Safadi',
+        cityCountry: 'Istanbul, Turkey',
+        email: 'veryvroosh@gmail.com',
+        github: 'github.com/veryvroosh',
+        linkedin: 'linkedin.com/in/mhd-zaid-safadi/'
+    })
+
+    function handleShowMore() {
+        setShowMore(!showMore)
+    }
+
     return (
         <div className="edit-card">
             <div className="main-card">
                 <h2>General</h2>
-                <button>V</button>
+                <button onClick={handleShowMore}>
+                    {showMore ? 'Hide' : 'Show'}
+                </button>
             </div>
+            {showMore && <ToggledGeneral form={form} setForm={setForm}/>}
         </div>
     )
 }
